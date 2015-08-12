23d1c3a1e2fb27f056fae2f899579cd2526b966a
Raphib737 <raph737@bu.edu>
2015-08-12 01:55:35 -0400

    database implementations

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -6,5 +6,14 @@ myApp.controller('AppCtrl',['$scope', '$http',
 			$scope.contactlist= response ;
 		});
 
+	$scope.addContact = function(){
+		console.log($scope.contact);
+		$http.post('/contactlist',$scope.contact).success(function (response){
+			console.log(response);	
+		});
+
+	};
+
+
 	}]);
 
--- server.js
+++ server.js
@@ -1,31 +1,23 @@
 var express = require('express');
 var app = express() ;
+var mongojs = require('mongojs');
+var db = mongojs('contactlist',['contactlist']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 app.get('/contactlist',function(req, res){
 	console.log("I recieved a GET request")
 
-	person1 = {
-		name: "Tim",
-		email: "tim@bu.edu",
-		number: '111-111-111'
-	};
+	db.contactlist.find(function (err,docs){
+		console.log(docs);
+		res.json(docs);
+	});
+});
 
-	person2 = {
-		name: "im",
-		email: "im@bu.edu",
-		number: '222-222-222'
-	};
-
-	person3 = {
-		name: "m",
-		email: "m@bu.edu",
-		number: '333-333-333'
-	};
-
-	var contactlist = [person1,person2,person3];
-	res.json(contactlist);
+app.post('/contactlist', function (req, res){
+	console.log(req.body);
 });
 
 app.listen(3000);
